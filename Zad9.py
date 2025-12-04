f0e7c9db1ade48e17f471dae3d2802162d5ba823
Ivan <kovalenkoivol9@gmail.com>
2025-12-04 19:50:52 +0100

    Zad 9

---
+++ Zad9.py
@@ -0,0 +1,13 @@
+# Zadanie 9
+
+zakupy = {
+    "chleb": 4.50,
+    "masło": 8.20,
+    "ser": 6.40,
+    "mleko": 3.20
+}
+
+print("Lista zakupów:", zakupy)
+
+suma = sum(zakupy.values())
+print("Suma wydatków:", suma)
