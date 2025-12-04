0e9abe04e6976bbbe26685a46b74da5211acd029
Ivan <kovalenkoivol9@gmail.com>
2025-12-04 19:38:20 +0100

    Zad 7

---
+++ Zad7.py
@@ -0,0 +1,9 @@
+# Zadanie 7
+
+moja_krotka = (10, 2, 6, 6, 9, 13, 0, 1)
+
+lista = list(moja_krotka)
+lista.sort()
+posortowana_krotka = tuple(lista)
+
+print("Posortowana krotka:", posortowana_krotka)
